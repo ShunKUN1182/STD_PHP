789a15dbb2ff74ff5352ecf58b07e0676495523c
ShunKUN1182 <2250378@ecc.ac.jp>
2026-06-05 17:34:49 +0900

    サンプル終了

--- samples/sample07-2.php
+++ samples/sample07-2.php
@@ -1,20 +1,53 @@
 <?php
   // アップロードしたファイル情報
-  var_dump($_FILES);
+  // var_dump($_FILES);
   // アップロードファイル情報
   $files = $_FILES["upfile"];
-  // ファイルをアップロードするフォルダがあるかチェック
-  // なければ、フォルダを生成する
-  if (!is_dir("../files/upload_images")) {
-    // フォルダの生成
-    mkdir("../files/upload_images");
-    // フォルダの権限設定
-    chmod("../files/upload_images",0777);
-  }
   // アップロードしたファイルのチェック
   if (is_uploaded_file($files["tmp_name"])) {
+    // 重複しないファイル名の生成(トークン生成)
+    // ## ローテクバージョン
+    // $filename = "";
+    // $patternStrings = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    // $length = 15;
+    // $flag = true;
+    // while($flag) {
+    //   for ($i = 0; $i < $length; $i++) { 
+    //     $filename .= substr($patternStrings,rand(0,strlen($patternStrings)-1),1);
+    //   };
+    //   $flag = false;
+    //   // 生成したトークンの強度チェック
+    //   if (
+    //     !preg_match("/[a-z]+/",$filename) ||
+    //     !preg_match("/[A-Z]+/",$filename) ||
+    //     !preg_match("/[0-9]+/",$filename)
+    //     ) {
+    //       print "英小文字なし";
+    //       $flag = true;
+    //     }
+    // }
+    // print $filename;
+    
+    // ファイルをアップロードするフォルダがあるかチェック
+    // なければ、フォルダを生成する
+
+    // ##ハイテクバージョン
+    $filename = base64_encode(random_bytes(15));
+    // 不要な記号を削除
+    $filename = str_replace(["=","+","/","."], "", $filename);
+    $filename = substr($filename,0,15);
+    print $filename . "(" . strlen($filename) . ")";
+
+
+
+    if (!is_dir("../files/upload_images")) {
+      // フォルダの生成
+      mkdir("../files/upload_images");
+      // フォルダの権限設定
+      chmod("../files/upload_images",0777);
+    }
     // アップロードしたファイルを所定のフォルダへ移動
-    if (move_uploaded_file($files["tmp_name"],"../files/upload_images/{$files["name"]}")) {
+    if (move_uploaded_file($files["tmp_name"],"../files/upload_images/{$filename}.png")) {
       print "アップロード先に移動したで";
     }else {
       print "アップロード先に移動できんかった";
